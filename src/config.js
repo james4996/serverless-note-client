ae67218862128fb8e1d911a47ca94329d9c28ed8
James <james@tantami.co.uk>
2019-11-07 21:03:25 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "tantami-notes-serve"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://uchrzojv3f.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_YPUrlrnQm",
+      APP_CLIENT_ID: "4h393b1ifksr8tbm0knj0s3jqc",
+      IDENTITY_POOL_ID: "eu-west-1:7043f1aa-6f69-4fa2-b0e1-1d25eb01815e"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Tantami Dev</h1>
+        <p>Note taking app</p>
+      </div>
+    </div>
+  );
+}
