873be874dde2589af3e459723bc35b53d39898ab
James <james@tantami.co.uk>
2019-11-07 21:55:23 +0000

    Added Login

--- src/index.js
+++ src/index.js
@@ -9,27 +9,27 @@ import config from './config';
 
 Amplify.configure({
     Auth: {
-      mandatorySignIn: true,
-      region: config.cognito.REGION,
-      userPoolId: config.cognito.USER_POOL_ID,
-      identityPoolId: config.cognito.IDENTITY_POOL_ID,
-      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
     },
     Storage: {
-      region: config.s3.REGION,
-      bucket: config.s3.BUCKET,
-      identityPoolId: config.cognito.IDENTITY_POOL_ID
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
     },
     API: {
-      endpoints: [
+        endpoints: [
         {
-          name: "notes",
-          endpoint: config.apiGateway.URL,
-          region: config.apiGateway.REGION
+            name: "notes",
+            endpoint: config.apiGateway.URL,
+            region: config.apiGateway.REGION
         },
-      ]
+        ]
     }
-  });
+});
 
 ReactDOM.render(
     <Router>
@@ -38,7 +38,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
